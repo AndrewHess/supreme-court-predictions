62f9c1692da59d707bc5006e01881ea8b621af82
Andrew Hess <ahess3815@gmail.com>
2018-05-14 17:11:49 -0700

    modified so that data files are one csv file for each class

--- util/format_data.py
+++ util/format_data.py
@@ -6,22 +6,49 @@ counters = [0] * 4  # File number counters for train and test, 1 and 2
 random.seed(3)
 
 # Open the files
-data = open('../data/numeric.csv', 'r')
-result = open('../data/decision.csv', 'r')
+data = open('../data.nosync/features.csv', 'r')
+result = open('../data.nosync/labels.csv', 'r')
+
+train0 = open('../data.nosync/train/0/data.csv', 'w+')
+train1 = open('../data.nosync/train/1/data.csv', 'w+')
+test0 = open('../data.nosync/test/0/data.csv', 'w+')
+test1 = open('../data.nosync/test/1/data.csv', 'w+')
 
 for outcome in result:
     outcome = outcome.strip()  # Remove whitespace
 
     # Ignore outcomes that are not 1 or 2
-    if (len(outcome) == 0 or int(outcome) > 2):
+    if (len(outcome) == 0 or int(outcome) > 1):
         continue
 
     # Randomly add this case to either test or train.
-    filename = '../data/test/'
+    filename = None
+
+    if (random.random() > test_size):
+        if (outcome == '0'):
+            filename = train0
+        elif (outcome == '1'):
+            filename = train1
+        else:
+            print('unknown outcome:', outcome)
+    else:
+        if (outcome == '0'):
+            filename = test0
+        elif (outcome == '1'):
+            filename = test1
+        else:
+            print('unknown outcome:', outcome)
+
+    # Now that we have the correct file, we just write it.
+    filename.write(data.readline())
+
+    '''
+    # Randomly add this case to either test or train.
+    filename = '../data.nosync/test/'
     train = False
 
     if (random.random() > test_size):
-        filename = '../data/train/'
+        filename = '../data.nosync/train/'
         train = True
 
     # Add the case to the correct folder.
@@ -35,8 +62,13 @@ for outcome in result:
     # Put this case in the appropriate file.
     with open(filename, 'w+') as new_file:
         new_file.write(data.readline())
+    '''
 
 
 # Clean up the files
 result.close()
 data.close()
+train0.close()
+train1.close()
+test0.close()
+test1.close()
