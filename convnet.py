b6411dca928514e7f318c46d679b725545cf038a
Andrew Hess <ahess3815@gmail.com>
2018-05-21 10:03:38 -0700

    modified the neural network without much improvement

--- convnet.py
+++ convnet.py
@@ -5,94 +5,17 @@ import numpy as np
 
 # Get the data.
 train_x, train_y, test_x, test_y = split_data()
-'''
-print('------------------ test x ------------------')
-print(test_x)
-print('------------------ test y ------------------')
-print(test_y)
-for item in test_y:
-    print(test_y)
-
-print('train_y shape:', train_y.shape)
-for i in range(train_y.shape[0]):
-    print(train_y[i][0], end=' ')
-'''
-
 
 # Create the model.
 model = Sequential()
-'''
-model.add(Conv1D(16, 4, input_shape=(11, 1)))
-model.add(Activation('relu'))
-#model.add(MaxPooling1D())
-
-model.add(Conv1D(16, 4))
-model.add(Activation('relu'))
-#model.add(MaxPooling1D())
-
-model.add(Conv1D(32, 4))
-model.add(Activation('relu'))
-#model.add(MaxPooling1D())
-'''
-
-'''
-model.add(Dense(16, input_shape=(10, 1)))
-model.add(Dense(32))
-model.add(Activation('relu'))
-model.add(Dense(32))
-model.add(Dense(64))
-model.add(Activation('relu'))
-
-model.add(Dense(128))
-model.add(Dense(256))
-model.add(Activation('relu'))
-model.add(Dense(256))
-model.add(Conv1D(128, 4))
-model.add(Dense(256))
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dense(64))
-
-model.add(Flatten())
-model.add(Dense(32))
-model.add(Activation('relu'))
-#model.add(Dropout(0.5))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-'''
-
-'''
-model.add(Dense(16, input_shape=(10, 1)))
-model.add(Activation('relu'))
-
-model.add(Dense(32))
-model.add(Dense(32))
-model.add(Activation('relu'))
-model.add(Dense(64))
-model.add(Dense(128))
-model.add(Dense(256))
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dense(128))
-model.add(Dense(64))
-model.add(Dense(32))
-model.add(Dense(16))
-model.add(Dense(10))
-
-model.add(Flatten())
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-'''
 
 model.add(Dense(32, input_shape=(149, 1)))
 model.add(Dense(16))
 model.add(Dense(16))
 model.add(Dense(8))
-
 model.add(Flatten())
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
-
 
 model.summary()
 
