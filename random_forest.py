04bc01ddcaae37286b6773aa1ef7a66c48535261
Andrew Hess <ahess3815@gmail.com>
2018-05-21 17:22:44 -0700

    setup random forest classifier

---
+++ random_forest.py
@@ -0,0 +1,13 @@
+from util.data import split_data
+from sklearn.ensemble import RandomForestClassifier
+
+# Get the data.
+train_x, train_y, test_x, test_y = split_data(dim=2)
+
+# Create the model.
+clf = RandomForestClassifier(n_estimators=50, criterion='entropy', max_features=None, min_samples_split=10, n_jobs=-1)
+clf.fit(train_x, train_y)
+
+# Test the model.
+print('Train Accuracy:', clf.score(train_x, train_y))
+print('Test Accuracy:', clf.score(test_x, test_y))
